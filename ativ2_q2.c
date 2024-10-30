9de6a42bfdae1ca62969e5f8cd5c200566351f14
MarinaSMarques <166162970+MarinaSMarques@users.noreply.github.com>
2024-10-30 18:30:41 -0300

    Create ativ2_q2.c

---
+++ ativ2_q2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h> 
+int main() 
+{ 
+    //code 
+    int a; 
+    int b; 
+    int soma; 
+    int multiplicacao; 
+    printf("Entre com o primeiro número"); 
+    scanf("%d", &a); 
+    printf("Entre com o segundo número"); 
+    scanf("%d", &b); 
+    if ((a>=0) && (b>=0)) 
+    if (a<b) 
+    soma=(a+b); 
+    multiplicacao=(soma*3); 
+    {
+    printf("%d", multiplicacao); 
+    } 
+return 0; 
+}
